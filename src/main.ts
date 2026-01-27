d371166d55daea4339331651de3279916c895ed3
Promise <i@xenlyra.cc>
2026-01-27 23:10:24 +0800

    initialize repo

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(createVuetify())
+app.use(router)
+
+app.mount('#app')

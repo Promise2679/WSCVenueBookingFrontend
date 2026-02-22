0bdb823e595d5f73bbf4f66b710c85541c816479
Promise <i@xenlyra.cc>
2026-02-22 13:20:19 +0800

    微调首页逻辑

--- src/constants/tabs.ts
+++ src/constants/tabs.ts
@@ -1,10 +1,12 @@
 interface HomeTab {
+  color: string | undefined
   icon: string
   name: string
   path: string | undefined
 }
 
 export const homeTabs: HomeTab[] = [
-  { icon: 'mdi-email', name: 'notice', path: '/notice' },
-  { icon: 'mdi-cog', name: 'setting', path: '/setting' }
+  { color: undefined, icon: 'mdi-email', name: 'notice', path: '/notice' },
+  { color: undefined, icon: 'mdi-cog', name: 'setting', path: '/setting' },
+  { color: 'red', icon: 'mdi-exit-to-app', name: 'exit', path: undefined }
 ]
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,22 +1,17 @@
 <template>
   <v-app id="inspire">
     <v-app-bar class="px-3" density="comfortable">
-      <v-icon icon="access-point" size="large"></v-icon>
-      <v-spacer></v-spacer>
+      <v-icon icon="access-point" size="large" />
+      <v-spacer />
       <v-tabs align-tabs="center" color="grey-darken-2">
-        <v-tab v-for="tab in homeTabs" :key="tab.name" :to="tab.path">
-          <v-icon :icon="tab.icon" size="large"></v-icon>
-        </v-tab>
-      </v-tabs>
-      <v-tabs align-tabs="center" color="grey-darken-2">
-        <v-tab color="red" hide-slider>
-          <v-icon icon="mdi-exit-to-app" size="large" color="red"></v-icon>
+        <v-tab v-for="tab in homeTabs" :key="tab.name" :to="tab.path" hide-slider :color="tab.color">
+          <v-icon :icon="tab.icon" :color="tab.color" size="large" />
         </v-tab>
       </v-tabs>
     </v-app-bar>
 
     <v-main class="bg-grey-lighten-3">
-      <RouterView></RouterView>
+      <RouterView />
     </v-main>
   </v-app>
 </template>

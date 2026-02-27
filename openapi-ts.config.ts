ddb9aea0d638f9a6a38ccc30f7fff4f43d308f64
Promise <i@xenlyra.cc>
2026-02-27 19:53:32 +0800

    cross origin

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,9 +3,5 @@ import { defineConfig } from '@hey-api/openapi-ts'
 export default defineConfig({
   input: 'openapi.yaml',
   output: 'src/api',
-  plugins: [
-    '@hey-api/sdk',
-    { baseUrl: 'http://127.0.0.1:4523/m1/7792878-7539603-default', name: '@hey-api/client-fetch' },
-    '@pinia/colada'
-  ]
+  plugins: ['@hey-api/sdk', '@hey-api/client-fetch', '@pinia/colada']
 })
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ import vuetify from 'vite-plugin-vuetify'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), vuetify()],
-  resolve: { alias: { '@': fileURLToPath(new URL('src', import.meta.url)) } }
+  resolve: { alias: { '@': fileURLToPath(new URL('src', import.meta.url)) } },
+  server: { proxy: { '/api': { changeOrigin: true, target: 'http://154.64.230.227:8800' } } }
 })

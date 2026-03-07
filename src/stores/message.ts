e889122914cd9f5adb916768fcdd2fffc895ebc9
Promise <i@xenlyra.cc>
2026-03-07 14:20:18 +0800

    feat: 全局消息队列支持 timeout，并设置默认值

--- src/stores/message.ts
+++ src/stores/message.ts
@@ -4,13 +4,15 @@ import { ref } from 'vue'
 interface Msg {
   color: 'error' | 'success'
   text: string
+  timeout?: number
 }
 
 export const useMessagesStore = defineStore('message', () => {
   const queue = ref<Msg[]>([])
 
   function add(message: Msg) {
-    queue.value.push(message)
+    if (!message.timeout) message = { ...message, timeout: 2000 }
+    queue.value.push({ ...message })
   }
 
   return { add, queue }

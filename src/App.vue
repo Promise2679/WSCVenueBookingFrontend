86b2fc6454b6b8a8d34748d7e67455f283b57167
Promise <i@xenlyra.cc>
2026-03-08 16:02:17 +0800

    style: 调整错误拦截器位置

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,19 @@
 </template>
 
 <script setup lang="ts">
+import { client } from './api/client.gen'
+import router from './router'
 import { useMessagesStore } from './stores/message'
+import { useUserStore } from './stores/user'
 
 const messages = useMessagesStore()
+
+client.interceptors.error.use(async res => {
+  messages.add({ color: 'error', text: res.msg })
+
+  if (res.code === 200402 || res.code === 200401) {
+    useUserStore().$reset()
+    await router.push('/login')
+  }
+})
 </script>
